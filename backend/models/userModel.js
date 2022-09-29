c80b05b1a7deaac98bbb25a6ebc4ae1f8bcfa394
nayimHossen <nayimh2015@gmail.com>
2022-09-30 01:38:01 +0600

    reset password with node mailler

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -63,6 +64,22 @@ userSchema.methods.getJWTToken = function () {
 // COMPAIR PASSWORD
 userSchema.methods.conpirePassword = async function (password) {
   return await bcryptjs.compare(password, this.password);
+};
+
+// Generating Password Reset Token
+userSchema.methods.getResetPasswordToken = function () {
+  // Generating Token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hashing and adding resetPasswordToken to userSchema
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+  return resetToken;
 };
 
 module.exports = mongoose.model("User", userSchema);
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,10 +4,14 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/userController");
 
 router.route("/register").post(registerUser);
 router.route("/login").get(loginUser);
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset").put(resetPassword);
 router.route("/logout").get(logoutUser);
 
 module.exports = router;
